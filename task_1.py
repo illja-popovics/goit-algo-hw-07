a4b0097df65c0cb13020d3b2985ee2a3de3001cf
illja-popovics <143592959+illja-popovics@users.noreply.github.com>
2024-01-24 10:13:37 +0100

    added tasks

---
+++ task_1.py
@@ -0,0 +1,34 @@
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+def insert(root, key):
+    if root is None:
+        return Node(key)
+    else:
+        if key < root.key:
+            root.left = insert(root.left, key)
+        else:
+            root.right = insert(root.right, key)
+    return root
+
+def find_max_value(root):
+    if root is None:
+        return None
+    
+    while root.right is not None:
+        root = root.right
+    
+    return root.key
+
+if __name__ == "__main__":
+    root = Node(5)
+    root = insert(root, 3)
+    root = insert(root, 2)
+    root = insert(root, 4)
+    root = insert(root, 7)
+    root = insert(root, 6)
+    root = insert(root, 8)
+    print(find_max_value(root))
